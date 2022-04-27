8eb75fd3b06cf6b017d06d4e46195e06e698a32a
EdgarJeremy <edgarpontoh3141@gmail.com>
2022-04-28 01:23:39 +0800

    (*) fix start.sh typo

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 pm2 start src/ --name "Jobdesk API";
-pm2 start src/jobs/auto-absent --name "Auto Absent";
-pm2 start src/jobs/notifier --name "Notifier Script";
+pm2 start src/jobs/auto-absent.js --name "Auto Absent";
+pm2 start src/jobs/notifier.js --name "Notifier Script";
 pm2 save
