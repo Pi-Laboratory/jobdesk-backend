6ab58f678b57cf98fe6a42cd5188ee248d69322c
EdgarJeremy <edgarpontoh3141@gmail.com>
2022-04-29 16:32:06 +0800

    (+) add srid to spot area

--- src/services/spots/spots.class.js
+++ src/services/spots/spots.class.js
@@ -1,4 +1,19 @@
 const { Service } = require('feathers-sequelize');
 
 exports.Spots = class Spots extends Service {
+    async create(data) {
+        data.area = setSRID(data.area, 4326);
+        console.log(data);
+        return super.create(data);
+    }
 };
+
+function setSRID(geometry, srid) {
+    return {
+        ...geometry,
+        crs: {
+            type: 'name',
+            properties: { name: `EPSG:${srid}` },
+        },
+    };
+}
